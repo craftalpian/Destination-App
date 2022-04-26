0906dd445d514275473bb35c799a48a16146a989
Xenosians <fio.salis@gmail.com>
2022-04-26 12:38:44 +0700

    Test connection to database

---
+++ dbconfig/index.js
@@ -0,0 +1,20 @@
+const Sequelize = require("sequelize")
+
+const sequelize = new Sequelize('destination_app','root','root', {
+    host:'localhost',
+    port:'3306',
+    dialect:'mysql'
+});
+
+async function testConnection(){
+    try{
+        await sequelize.authenticate();
+        console.log("Connected");
+        const[results, metadata] = await sequelize.querry("SELECT * FROM destination");
+        console.log(results);
+        }catch(err){
+            console.log("Can't connect to database!");
+    }
+}
+
+testConnection();
