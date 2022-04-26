7105917d9671ac73a1453aa3ab51c098b781d3c2
craftalpian <alfiananandaputra@gmail.com>
2022-04-27 00:05:33 +0700

    Models done

--- dbconfig/index.js
+++ dbconfig/index.js
@@ -1,19 +1,19 @@
 const Sequelize = require("sequelize")
 
-const sequelize = new Sequelize('destination_app','root','root', {
-    host:'localhost',
-    port:'3306',
-    dialect:'mysql'
+const sequelize = new Sequelize('destination_app', 'root', '', {
+    host: 'localhost',
+    port: '3306',
+    dialect: 'mysql'
 });
 
-async function testConnection(){
-    try{
+async function testConnection() {
+    try {
         await sequelize.authenticate();
         console.log("Connected");
-        const[results, metadata] = await sequelize.querry("SELECT * FROM destination");
+        const [results, metadata] = await sequelize.querry("SELECT * FROM destination");
         console.log(results);
-        }catch(err){
-            console.log("Can't connect to database!");
+    } catch (err) {
+        console.log("Can't connect to database!");
     }
 }
 
---
+++ src/models/index.js
@@ -0,0 +1,50 @@
+const { DataTypes, Sequelize } = require('sequelize');
+
+const SequelizeModel = new Sequelize(
+    'destination_app',
+    'root',
+    '',
+    {
+        host: 'localhost',
+        dialect: 'mysql',
+        timezone: '+07:00',
+    },
+);
+
+const Destinations = SequelizeModel.define('destinations', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+    },
+    name: {
+        type: DataTypes.STRING
+    },
+    image: {
+        type: DataTypes.STRING,
+    },
+    location: {
+        type: DataTypes.STRING,
+    },
+    website_url: {
+        type: DataTypes.STRING,
+    },
+    instagram_url: {
+        type: DataTypes.STRING,
+    },
+    status: {
+        type: DataTypes.INTEGER,
+    },
+    created_at: {
+        type: DataTypes.STRING,
+    },
+    updated_at: {
+        type: DataTypes.STRING,
+    },
+}, {
+    underscored: true,
+    freezeTableName: true,
+});
+
+module.exports = {
+    Destinations,
+}
