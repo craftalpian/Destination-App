8e8a28084baf03ea2879efc43c956c447de3ca56
craftalpian <alfiananandaputra@gmail.com>
2022-04-27 12:55:01 +0700

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,169 @@
-# Destination-App
+# Destination App
+Pada repository ini, dibuat REST API yang akan digunakan untuk memenuhi kebutuhan __[front-end](https://github.com/whoisgalih/destination-app)__. Projek __Destination App__ ditujukan untuk tugas Probation PROCLUB Telkom University.
+
+__Anggota kelompok:__
+1. Alfian Ananda Putra ([craftalpian](https://github.com/craftalpian))
+1. Alifio Y. A. S ([Xenosians](https://github.com/Xenosians))
+1. Galih Akbar Nugraha ([whoisgalih](https://github.com/whoisgalih))
+
+__Cara mendapatkan API KEY imgbb.com:__
+1. Lakukan [pendaftaran akun](https://imgbb.com) di __imgbb.com__
+1. Pergi ke halaman [https://api.imgbb.com/](https://api.imgbb.com/)
+1. Salin API-KEY yang muncul di halaman tersebut
+---
+
+## Usage ✌
+* __Menggunakan _repository_ ini:__
+```sh
+git clone https://github.com/craftalpian/Destination-App.git
+```
+Lalu dilanjutkan dengan
+```sh
+npm i
+```
+Dan buat file `.env` dengan code berikut
+```
+IMGBB_API=<YOUR-IMGBB-API-KEY>
+```
+
+
+* __Menjalankan saat _development_:__
+```sh
+npm run start-dev
+```
+
+* __Menjalankan saat _production_:__
+```sh
+npm run start-prod
+```
+
+## Documentation 📖
+* __[POST] Insert Data__ (/destinations)
+
+Untuk menambahkan destinasi wisata terbaru ke database.
+
+_Request data:_
+```
+{
+    "name": "Kota Depok",
+    "image": "iVBORw0KGgoAAAANSUhEUgAAAAIAAAACCAYAAABytg0kAAAAEklEQVR42mNcLVNbzwAEjDAGACcSA4kB6ARiAAAAAElFTkSuQmCC",
+    "location": "Depok, Jawa Barat, Indonesia",
+    "website_url": "https://depok.go.id/",
+    "instagram_url": "https://www.instagram.com/",
+    "description": "Merupakan kota penyanggah Ibu Kota"
+}
+```
+*image yang dikirim berupa base64 (gunakan [base64-image.de](https://www.base64-image.de/))
+
+_Response data:_
+```
+{
+    "status": "success",
+    "code": 200,
+    "message": "destinasi berhasil ditambahkan",
+    "data": []
+}
+```
+* __[PUT] Update Data__ (/destinations/{id})
+
+Untuk mengubah data destinasi wisata.
+
+_Request data:_
+```
+{
+    "name": "Kota Jakarta",
+    "image": "iVBORw0KGgoAAAANSUhEUgAAAAIAAAACCAYAAABytg0kAAAAEklEQVR42mNcLVNbzwAEjDAGACcSA4kB6ARiAAAAAElFTkSuQmCC",
+    "location": "Jakarta, Indonesia",
+    "website_url": "https://jakarta.go.id/",
+    "instagram_url": "https://www.instagram.com/",
+    "description": "Kota Besar"
+}
+```
+*image yang dikirim berupa base64 (gunakan [base64-image.de](https://www.base64-image.de/))
+
+_Response data:_
+```
+{
+    "status": "success",
+    "code": 200,
+    "message": "destinasi berhasil diupdate",
+    "data": []
+}
+```
+* __[GET] Get All Destination__ (/destinations)
+
+Untuk mendapatkan semua destinasi dari database.
+
+_Response data:_
+```
+{
+    "status": "success",
+    "code": 200,
+    "message": "mengambil semua data",
+    "data": [
+        {
+            "id": 1,
+            "name": "Kota Depok",
+            "image": "https://i.ibb.co/GWrRNrB/085074f6df93.png",
+            "location": "Depok, Jawa Barat, Indonesia.",
+            "website_url": "https://depok.go.id/",
+            "instagram_url": "https://www.instagram.com/",
+            "description": "Merupakan penyanggah Ibu Kota"
+        },
+        {
+            "id": 2,
+            "name": "Kota Jakarta",
+            "image": "https://i.ibb.co/GWrRNrB/085074f6df93.png",
+            "location": "Jakarta, Indonesia.",
+            "website_url": "https://jakarta.go.id/",
+            "instagram_url": "https://www.instagram.com/",
+            "description": ""
+        },
+    ]
+}
+```
+* __[GET] Get Specific Destination__ (/destinations/{id})
+
+Untuk mendapatkan data destinasi berdasarkan `id`.
+
+_Response data:_
+```
+{
+    "status": "success",
+    "code": 200,
+    "message": "mengambil data",
+    "data": {
+        "id": 1,
+        "name": "Kota Depok",
+        "image": "https://i.ibb.co/GWrRNrB/085074f6df93.png",
+        "location": "Depok, Jawa Barat, Indonesia.",
+        "website_url": "https://depok.go.id/",
+        "instagram_url": "https://www.instagram.com/",
+        "description": "Merupakan penyanggah Ibu Kota"
+    }
+}
+```
+* __[DELETE] Remove Specific Destination__ (/destinations/{id})
+
+Untuk menghapus data destinasi berdasarkan `id`.
+
+_Response data:_
+```
+{
+    "status": "success",
+    "code": 200,
+    "message": "destinasi berhasil dihapus",
+    "data": []
+}
+```
+
+## Tech 💻
+1. [HapiJs](https://hapi.dev/)
+1. MYSQL
+1. Sequelize
+1. [imgbb-uploader](https://www.npmjs.com/package/imgbb-uploader)
+1. dotenv
+
+## Requirements ✅
+1. [SQL file]()
+1. [POSTMAN file](https://github.com/craftalpian/Destination-App/blob/main/Destination%20App.postman_collection.json) to test API
---
+++ destination_app.sql
@@ -0,0 +1,88 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 27, 2022 at 07:26 AM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `destination_app`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `destinations`
+--
+
+CREATE TABLE `destinations` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_swedish_ci DEFAULT NULL,
+  `image` text COLLATE utf8_swedish_ci DEFAULT NULL,
+  `location` text COLLATE utf8_swedish_ci DEFAULT NULL,
+  `website_url` varchar(255) COLLATE utf8_swedish_ci DEFAULT NULL,
+  `instagram_url` varchar(255) COLLATE utf8_swedish_ci DEFAULT NULL,
+  `description` text COLLATE utf8_swedish_ci DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
+
+--
+-- Dumping data for table `destinations`
+--
+
+INSERT INTO `destinations` (`id`, `name`, `image`, `location`, `website_url`, `instagram_url`, `description`, `status`, `created_at`, `updated_at`) VALUES
+(2, 'Kota Jakarta', 'https://i.ibb.co/GWrRNrB/085074f6df93.png', 'Depok, Jawa Barat, Indonesia.', 'https://depok.go.id/', 'https://www.instagram.com/', 'Merupakan Ibu Kota', 1, '2022-04-27 02:05:14', '2022-04-27 04:51:17'),
+(3, 'Kota Depok', 'https://berita.depok.go.id/upload/media/entries/2021-11/04/9106-0-695e2dd4b1fb0700c3af7ecc16d81a78.jpg', 'Depok, Jawa Barat, Indonesia', 'https://depok.go.id/', 'https://www.instagram.com/', 'Merupakan kota penyanggah Ibu Kota', 1, '2022-04-27 02:05:57', '2022-04-27 02:05:57'),
+(4, 'Kota Depok', 'https://berita.depok.go.id/upload/media/entries/2021-11/04/9106-0-695e2dd4b1fb0700c3af7ecc16d81a78.jpg', 'Depok, Jawa Barat, Indonesia', 'https://depok.go.id/', 'https://www.instagram.com/', NULL, 1, '2022-04-27 02:06:04', '2022-04-27 02:06:04'),
+(5, 'Kota Depok', 'https://berita.depok.go.id/upload/media/entries/2021-11/04/9106-0-695e2dd4b1fb0700c3af7ecc16d81a78.jpg', 'Depok, Jawa Barat, Indonesia', 'https://depok.go.id/', 'https://www.instagram.com/', 'Merupakan kota penyanggah Ibu Kota', 1, '2022-04-27 02:08:31', '2022-04-27 02:08:31'),
+(6, 'Kota Depok', 'https://berita.depok.go.id/upload/media/entries/2021-11/04/9106-0-695e2dd4b1fb0700c3af7ecc16d81a78.jpg', 'Depok, Jawa Barat, Indonesia', 'https://depok.go.id/', 'https://www.instagram.com/', NULL, 1, '2022-04-27 02:08:38', '2022-04-27 02:08:38'),
+(7, 'Kota Depok', 'https://berita.depok.go.id/upload/media/entries/2021-11/04/9106-0-695e2dd4b1fb0700c3af7ecc16d81a78.jpg', 'Depok, Jawa Barat, Indonesia', 'https://depok.go.id/', 'https://www.instagram.com/', NULL, 1, '2022-04-27 02:09:58', '2022-04-27 02:09:58'),
+(8, 'Kota Depok', 'https://berita.depok.go.id/upload/media/entries/2021-11/04/9106-0-695e2dd4b1fb0700c3af7ecc16d81a78.jpg', 'Depok, Jawa Barat, Indonesia', 'https://depok.go.id/', 'https://www.instagram.com/', 'Merupakan kota penyanggah Ibu Kota', 1, '2022-04-27 02:10:05', '2022-04-27 02:10:05'),
+(9, 'Kota Depok', 'https://berita.depok.go.id/upload/media/entries/2021-11/04/9106-0-695e2dd4b1fb0700c3af7ecc16d81a78.jpg', 'Depok, Jawa Barat, Indonesia', 'https://depok.go.id/', 'https://www.instagram.com/', 'Merupakan kota penyanggah Ibu Kota', 1, '2022-04-27 02:12:24', '2022-04-27 02:12:24'),
+(10, 'Kota Depok', 'https://berita.depok.go.id/upload/media/entries/2021-11/04/9106-0-695e2dd4b1fb0700c3af7ecc16d81a78.jpg', 'Depok, Jawa Barat, Indonesia', 'https://depok.go.id/', 'https://www.instagram.com/', 'Merupakan kota penyanggah Ibu Kota', 1, '2022-04-27 02:12:37', '2022-04-27 02:12:37'),
+(11, 'Kota Depok 2', NULL, 'Depok, Jawa Barat, Indonesia', 'https://depok.go.id/', 'https://www.instagram.com/', 'Merupakan kota penyanggah Ibu Kota', 1, '2022-04-27 04:45:58', '2022-04-27 04:45:58'),
+(12, 'Kota Depok 2', NULL, 'Depok, Jawa Barat, Indonesia', 'https://depok.go.id/', 'https://www.instagram.com/', 'Merupakan kota penyanggah Ibu Kota', 1, '2022-04-27 04:46:57', '2022-04-27 04:46:57'),
+(13, 'Kota Depok 2', 'https://i.ibb.co/5rdxd8P/cfe44cec4ed2.png', 'Depok, Jawa Barat, Indonesia', 'https://depok.go.id/', 'https://www.instagram.com/', 'Merupakan kota penyanggah Ibu Kota', 1, '2022-04-27 04:47:14', '2022-04-27 04:47:14'),
+(14, 'Kota Depok 2', 'https://i.ibb.co/GWrRNrB/085074f6df93.png', 'Depok, Jawa Barat, Indonesia', 'https://depok.go.id/', 'https://www.instagram.com/', 'Merupakan kota penyanggah Ibu Kota', 1, '2022-04-27 04:47:35', '2022-04-27 04:47:35'),
+(15, 'Kota Depok 2', 'https://i.ibb.co/YtPZVtx/57e155ae70b0.png', 'Depok, Jawa Barat, Indonesia', 'https://depok.go.id/', 'https://www.instagram.com/', 'Merupakan kota penyanggah Ibu Kota', 1, '2022-04-27 04:57:10', '2022-04-27 04:57:10'),
+(16, 'Kota Depok 2', 'https://i.ibb.co/GWrRNrB/085074f6df93.png', 'Depok, Jawa Barat, Indonesia', 'https://depok.go.id/', 'https://www.instagram.com/', 'Merupakan kota penyanggah Ibu Kota', 1, '2022-04-27 04:58:47', '2022-04-27 04:58:47');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `destinations`
+--
+ALTER TABLE `destinations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `destinations`
+--
+ALTER TABLE `destinations`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
