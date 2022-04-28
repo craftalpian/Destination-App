6c6291ca0d56126460d682fd0b17241761a175d9
Galih Akbar <galihakbar.ga91@gmail.com>
2022-04-28 12:37:14 +0700

    enable cors

--- src/server.js
+++ src/server.js
@@ -11,31 +11,34 @@ const DestinationsAPI = require('./api/destinations');
 const DestinationsService = require('./services/DestinationsService');
 
 const init = async () => {
-    // Destinations service
-    const destinationsService = new DestinationsService();
+  // Destinations service
+  const destinationsService = new DestinationsService();
 
-    // Set HAPI server config
-    const server = Hapi.server({
-        port: 3000,
-        host: 'localhost',
-    });
+  // Set HAPI server config
+  const server = Hapi.server({
+    port: 3000,
+    host: 'localhost',
+    routes: {
+      cors: true,
+    },
+  });
 
-    // Register api
-    await server.register([
-        {
-            plugin: DestinationsAPI,
-            options: {
-                destinationsService,
-            }
-        }
-    ]);
+  // Register api
+  await server.register([
+    {
+      plugin: DestinationsAPI,
+      options: {
+        destinationsService,
+      },
+    },
+  ]);
 
-    // Start server
-    await server.start();
+  // Start server
+  await server.start();
 
-    // Server was running
-    console.log(`Server start on ${server.info.uri}`);
-}
+  // Server was running
+  console.log(`Server start on ${server.info.uri}`);
+};
 
 // Run
 init();
